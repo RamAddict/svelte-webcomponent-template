24374d4547bcb5abc613dcb281362c2e3fc04cb8
ramaddict <apbiancofriber@gmail.com>
2022-11-14 10:15:09 -0300

    Add basic communication example

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,11 @@
   import Counter from "./lib/Counter.svelte";
   /**@ts-ignore*/
   import css from "./assets/app.postcss";
+
+  // example exporting a function callable by the parent page
+  export function foo(param: string) {
+    console.log("received param: " + param)
+  };
 </script>
 
 <div class="font-sans">
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,9 +1,13 @@
 <svelte:options tag="counter-that-counts" />
 <script lang="ts">
-  let count: number = 0
+  let count: number = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+    // example sending an event to the parent page
+    window.dispatchEvent(
+      new CustomEvent("component-hello", { detail: { foo: "bar" } })
+    );
+  };
 </script>
 
 <button on:click={increment} class="btn btn-primary">
